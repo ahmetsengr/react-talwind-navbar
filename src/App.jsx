81274357fb12dbbf1c37fce7239c0f42e81e1435
ahmetsengr <ahmetgg00@hotmail.com>
2023-07-02 22:20:39 +0300

    descrition
    you can run this web project just by opening the cmd, using the CD command to the corresponding file location and going to "npm install" and saying npm start. happy hacking :)

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Section from "./components/Section";
+
+function App() {
+  return (
+    <div className=''>
+      <Header/>
+      <Section/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Section.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Section = () => {
+  return (
+    <div className='bg-hero-image w-full h-screen bg-cover bg-center flex items-center px-4'>
+        <div className='uppercase text-center text-white font-extrabold  bg-black/50'>
+            <h3 className='text-4xl '>Hi im Ahmet sengor</h3>
+            <h3>Im a Frontend Developer</h3>
+        </div>
+    </div>
+  );
+};
+
+export default Section;
